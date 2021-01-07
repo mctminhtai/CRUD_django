4dc37d115f44a9a772a317c7c913039604e064f4
mctminhtai <mct.minhtai@gmail.com>
2021-01-07 10:51:40 +0700

    sua label, empty_labe, xoa required

---
+++ employee_register/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('',employee_form,name='employee_insert'),
+    path('<int:id>/',employee_form,name='employee_update'),
+    path('delete/<int:id>/',employee_delete,name='employee_delete'),
+    path('list/',employee_list,name='employee_list'),
+]
